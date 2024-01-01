e7d7df2fe725f206dff748a94b3c2b4fb89a0474
patelyash9775 <yashpatel9775@gmail.com>
2024-01-01 11:29:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.css"
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import useClipboard from "react-use-clipboard";
+import {useState} from "react";
+
+
+const App = () => {
+    const [textToCopy, setTextToCopy] = useState();
+   
+
+
+    const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
+  const { transcript, resetTranscript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+   const [isCopied, setCopied] = useClipboard(transcript, {
+        successDuration:1000
+    });
+
+    if (!browserSupportsSpeechRecognition) {
+        return null
+    }
+
+    return (
+        <>
+            <div className="container">
+                <h2>Speech to Text Converter</h2>
+                <br/>
+                <p>Welcome to Yash's React Application. Hope you enjoy well with this one.</p>
+
+                <div className="main-content">
+                    {transcript}
+                </div>
+
+                <div className="btn-style">
+
+                    <button onClick={setCopied}>
+                        {isCopied ? 'Copied!' : 'Copy to clipboard'}
+                    </button>
+                    <button onClick={startListening}>Start Listening</button>
+            <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
+            <button onClick={resetTranscript}>Clear Text</button>
+
+                </div>
+
+            </div>
+
+        </>
+    );
+};
 
 export default App;
